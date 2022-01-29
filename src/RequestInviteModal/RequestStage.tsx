b5c0542c3aa97703a1b225cda9ce490d826df106
pwb1997 <pwb1997@hotmail.com>
2022-01-29 17:08:13 +0800

    feat: implement layout for request invite modal

---
+++ src/RequestInviteModal/RequestStage.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+
+interface RequestStageProps {
+    isVisible: boolean;
+    onSuccess: () => void;
+}
+
+const RequestStage = ({ isVisible, onSuccess }: RequestStageProps) => {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [confirmEmail, setConfirmEmail] = useState('');
+    const [errorMsg] = useState('this is an error');
+
+    const submitRequest = () => {
+        onSuccess();
+    };
+
+    const inputClassName = 'px-4 py-2 w-full border border-black';
+
+    return isVisible ? (
+        <div className="flex flex-col items-center w-full h-full gap-8">
+            <span className="text-2xl italic font-bold text-gray-500">Request an invite</span>
+            <div className="w-8 border-t border-gray-500" />
+            <form
+                className="flex flex-col items-center flex-1 w-full"
+                onSubmit={() => submitRequest()}
+            >
+                <div className="flex flex-col justify-center flex-1 w-full gap-4">
+                    <input
+                        className={inputClassName}
+                        placeholder="Full name"
+                        type="text"
+                        value={fullName}
+                        onChange={(ev) => setFullName(ev.target.value)}
+                    />
+                    <input
+                        className={inputClassName}
+                        placeholder="Email"
+                        type="text"
+                        value={email}
+                        onChange={(ev) => setEmail(ev.target.value)}
+                    />
+                    <input
+                        className={inputClassName}
+                        placeholder="Confirm email"
+                        type="text"
+                        value={confirmEmail}
+                        onChange={(ev) => setConfirmEmail(ev.target.value)}
+                    />
+                </div>
+                <button
+                    className="w-full px-4 py-2 mt-12 text-sm font-bold text-gray-500 border border-gray-500"
+                    type="submit"
+                >
+                    Send
+                </button>
+                <div className="flex items-center justify-center h-8 text-sm font-bold text-gray-500 ">
+                    {errorMsg}
+                </div>
+            </form>
+        </div>
+    ) : null;
+};
+
+export default RequestStage;
---
+++ src/RequestInviteModal/SuccessStage.tsx
@@ -0,0 +1,26 @@
+interface SuccessStageProps {
+    isVisible: boolean;
+    onOk: () => void;
+}
+
+const SuccessStage = ({ isVisible, onOk }: SuccessStageProps) => {
+    return isVisible ? (
+        <div className="flex flex-col items-center w-full h-full gap-8">
+            <span className="text-2xl italic font-bold text-gray-500">All done!</span>
+            <div className="w-8 border-t border-gray-500" />
+            <div className="flex items-center justify-center flex-1">
+                <p className="text-center text-gray-500">
+                    You will be one of the first to experience Broccoli & Co. when we launch.
+                </p>
+            </div>
+            <button
+                className="w-full px-4 py-2 mb-8 text-sm font-bold text-gray-500 border border-gray-500"
+                onClick={() => onOk()}
+            >
+                OK
+            </button>
+        </div>
+    ) : null;
+};
+
+export default SuccessStage;
