cfd2640032a2a05099ed62cc04b11d192df276d5
pwb1997 <pwb1997@hotmail.com>
2022-02-05 23:16:37 +0800

    fix: fix mobile viewport height

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// fix mobile screen height
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
